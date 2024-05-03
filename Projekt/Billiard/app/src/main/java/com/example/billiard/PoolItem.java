194e55f06a7d6c9f72c58a70bc521b1650cd2ff1
mpisti <marik.istvan@stud.u-szeged.hu>
2024-05-03 15:08:08 +0200

    #2

--- Projekt/Billiard/app/src/main/java/com/example/billiard/PoolItem.java
+++ Projekt/Billiard/app/src/main/java/com/example/billiard/PoolItem.java
@@ -1,4 +1,35 @@
 package com.example.billiard;
 
 public class PoolItem {
+    private String name;
+    private int cue;
+    private int price;
+    private boolean free;
+
+    public PoolItem(String name, int cue, int price, boolean free) {
+        this.name = name;
+        this.cue = cue;
+        this.price = price;
+        this.free = free;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCue() {
+        return cue;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setFree(boolean free) {
+        this.free = free;
+    }
+
+    public boolean isFree() {
+        return free;
+    }
 }
